dc1ddf71841f7f470e70fa40b4f7f5366f37b186
John <john-obla@live.co.uk>
2020-08-23 22:29:06 +0100

    Deleted shop app

--- shop/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from .views import HomePageView, FoodListView
-
-urlpatterns = [
-    path('', HomePageView.as_view(), name='home'),
-    path('food/', FoodListView.as_view(), name='food'),
-]
