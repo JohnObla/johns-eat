f42a06ca5b71e66eb3bea10e8fd045941104465f
John <john-obla@live.co.uk>
2020-08-23 20:15:57 +0100

    Created food models, views, and templates

--- shop/urls.py
+++ shop/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import HomePageView
+from .views import HomePageView, FoodListView
 
 urlpatterns = [
-    path('', HomePageView.as_view(), name='home')
+    path('', HomePageView.as_view(), name='home'),
+    path('food/', FoodListView.as_view(), name='food'),
 ]
--- shop/views.py
+++ shop/views.py
@@ -1,5 +1,12 @@
-from django.views.generic import TemplateView
+from django.views.generic import TemplateView, ListView
+from .models import Food
 
 
 class HomePageView(TemplateView):
     template_name = 'home.html'
+
+
+class FoodListView(ListView):
+    model = Food
+    context_object_name = 'food_list'
+    template_name = 'food_list.html'
