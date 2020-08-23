10c76344f5ca5b81fe6a0e8563582e6ee2f5f629
John <john-obla@live.co.uk>
2020-08-24 00:17:07 +0100

    Registed models in admin

--- restaurants/admin.py
+++ restaurants/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Address, Allergy, Category, Cuisine, Food, Restaurant
 
-# Register your models here.
+admin.site.register(Address)
+admin.site.register(Allergy)
+admin.site.register(Category)
+admin.site.register(Cuisine)
+admin.site.register(Food)
+admin.site.register(Restaurant)
