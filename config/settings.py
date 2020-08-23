00d60056528bad1e70f9283a29f5125ba3fa6e68
John <john-obla@live.co.uk>
2020-08-23 16:48:24 +0100

    Created CustomUser Model

--- config/settings.py
+++ config/settings.py
@@ -50,6 +50,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    # Local
+    'accounts',
 ]
 
 MIDDLEWARE = [
@@ -129,3 +132,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+AUTH_USER_MODEL = 'accounts.CustomUser'
