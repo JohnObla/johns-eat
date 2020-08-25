7c1a1e5a604a9882f490677635c2bc9008197fba
John <john-obla@live.co.uk>
2020-08-25 21:10:59 +0100

    Saving everything for deployment

--- config/urls.py
+++ config/urls.py
@@ -19,5 +19,10 @@ from django.urls import path, include
 urlpatterns = [
     # Django admin
     path('behind-the-scenes/', admin.site.urls),
+
+    # User management
+    path('accounts/', include('django.contrib.auth.urls')),
+
+    # Local apps
     path('', include('restaurants.urls')),
 ]
